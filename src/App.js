64b2c9a7f5103748351ae08d9f1edd4a29a2b5ab
Jay Patwardhan <jaypatwardhan@Jays-MacBook-Pro.local>
2021-01-02 18:01:36 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/components';
+import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Pages/index'
 import Signup from './Pages/Signup'
